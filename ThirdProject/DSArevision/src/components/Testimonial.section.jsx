8955a39e6d5865a0355e73eef7d56990f31cfba8
Vishal Chauhan <vishal2825m@gmail.com>
2024-07-01 19:10:31 +0530

    Complete

--- ThirdProject/DSArevision/src/components/Testimonial.section.jsx
+++ ThirdProject/DSArevision/src/components/Testimonial.section.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from "react";
+import headImg from "../assets/profiles/profile-1.png";
 
 function TestimonialSection() {
   return (
-    <div>Testimonial.section</div>
-  )
+    <div className=" w-[80vw] m-auto py-7 px-20 grid grid-cols-2  gap-x-96  lg:gap-12  lg:w-10/12 ">
+        <div class="card border-2 border-gray-300 rounded px-8 py-7 w-[400px] m-5  lg:w-[600px] lg:py-15 ">
+          <p className="tracking-widest text-lg text-gray-500 sm:text-sm ">
+            “Walling helps us{" "}
+            <span className="font-bold">
+              visually organise ideas and tasks,
+            </span>{" "}
+            work collaboratively and efficiently within our team. The visual
+            experience makes it so easy to plan content and stay on track with
+            our projects and campaigns.”
+          </p>
+          <div className="flex items-center gap-10  mt-4">
+            <img src={headImg} className="h-16" />
+            <div>
+              <h1 className="text-green-500 text-sm lg:text-lg">Vishal Chauhan</h1>
+              <p className="text-gray-500 text-sm">Head of Development</p>
+            </div>
+          </div>
+        </div>
+        <div class="card border-2 border-gray-300 rounded px-8 py-7 w-[400px] m-5  lg:w-[600px] lg:py-15 ">
+          <p className="tracking-widest text-lg text-gray-500 sm:text-sm ">
+            “Walling helps us{" "}
+            <span className="font-bold">
+              visually organise ideas and tasks,
+            </span>{" "}
+            work collaboratively and efficiently within our team. The visual
+            experience makes it so easy to plan content and stay on track with
+            our projects and campaigns.”
+          </p>
+          <div className="flex items-center gap-10  mt-4">
+            <img src={headImg} className="h-16" />
+            <div>
+              <h1 className="text-green-500 text-sm lg:text-lg">Vishal Chauhan</h1>
+              <p className="text-gray-500 text-sm">Head of Development</p>
+            </div>
+          </div>
+        </div>
+        <div class="card border-2 border-gray-300 rounded px-8 py-7 w-[400px] m-5  lg:w-[600px] lg:py-15 ">
+          <p className="tracking-widest text-lg text-gray-500 sm:text-sm ">
+            “Walling helps us{" "}
+            <span className="font-bold">
+              visually organise ideas and tasks,
+            </span>{" "}
+            work collaboratively and efficiently within our team. The visual
+            experience makes it so easy to plan content and stay on track with
+            our projects and campaigns.”
+          </p>
+          <div className="flex items-center gap-10  mt-4">
+            <img src={headImg} className="h-16" />
+            <div>
+              <h1 className="text-green-500 text-sm lg:text-lg">Vishal Chauhan</h1>
+              <p className="text-gray-500 text-sm">Head of Development</p>
+            </div>
+          </div>
+        </div>
+        <div class="card border-2 border-gray-300 rounded px-8 py-7 w-[400px] m-5  lg:w-[600px] lg:py-15 ">
+          <p className="tracking-widest text-lg text-gray-500 sm:text-sm ">
+            “Walling helps us{" "}
+            <span className="font-bold">
+              visually organise ideas and tasks,
+            </span>{" "}
+            work collaboratively and efficiently within our team. The visual
+            experience makes it so easy to plan content and stay on track with
+            our projects and campaigns.”
+          </p>
+          <div className="flex items-center gap-10  mt-4">
+            <img src={headImg} className="h-16" />
+            <div>
+              <h1 className="text-green-500 text-sm lg:text-lg">Vishal Chauhan</h1>
+              <p className="text-gray-500 text-sm">Head of Development</p>
+            </div>
+          </div>
+        </div>
+      
+       
+      
+     
+    
+    
+    </div>
+  );
 }
 
-export default TestimonialSection
+export default TestimonialSection;
